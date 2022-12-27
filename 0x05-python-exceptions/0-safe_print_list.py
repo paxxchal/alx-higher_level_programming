e35bb191a59c01ecc0a30dccc4aafadf6100162a
paxxchal <paschalgabrielacj8@gmail.com>
2022-12-27 19:22:14 +0100

    added some exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
                 count += 1
         print()
         return count
-    except:
+    except IndexError:
         print("Oops! Something went wrong")
