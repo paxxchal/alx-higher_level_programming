a6a4af9d64afeebd7864b2ff5b92b478a680856f
paxxchal <paschalgabrielacj8@gmail.com>
2022-12-27 19:09:19 +0100

    Corrected some errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
-        while count < x:
-            for i in my_list:
-                print(i)
+        for i in my_list:
+            if count < x:
+                print(i, end="")
                 count += 1
+        print()
         return count
     except:
         print("Oops! Something went wrong")
