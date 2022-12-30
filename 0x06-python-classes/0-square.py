d1610bd3a48ff36da867f6cad8e0f6944b489bad
paxxchal <paschalgabrielacj8@gmail.com>
2022-12-30 19:24:02 +0100

    Did first class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""This module hosts an empty class, Square that defines a square"""
+
+
+class Square:
+    """This class describes a square"""
+
+    pass
