486462de8b50f50d33f2f5544d626264686c2c25
paxxchal <paschalgabrielacj8@gmail.com>
2023-01-06 04:13:15 +0100

    Did more tasks

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""This module hosts a function that prints a text
+with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """This function prints a text
+    with 2 new lines after each of these characters: ., ? and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == " ":
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == " ":
+                c += 1
+            continue
+        c += 1
