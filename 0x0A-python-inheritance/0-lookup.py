c418822f7cf145df3bc09847494259bfade4930f
paxxchal <paschalgabrielacj8@gmail.com>
2023-01-12 23:28:55 +0100

    fixed some bugs

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-"""This module retrieves a list of available attributes and methods of an object"""
+"""
+This module retrieves a list of
+available attributes and methods of an object
+"""
 
 
 def lookup(obj):
-    """This function returns a list of available attributes and methods of an object"""
+    """
+    This function returns a list of
+    available attributes and methods of an object
+    """
     List = []
     for i in dir(obj):
         List.append(i)
