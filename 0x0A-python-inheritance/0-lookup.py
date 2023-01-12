a0e3c833fa5b4a3ba1bf447478640301d97685e9
paxxchal <paschalgabrielacj8@gmail.com>
2023-01-12 23:25:00 +0100

    Did a task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""This module retrieves a list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """This function returns a list of available attributes and methods of an object"""
+    List = []
+    for i in dir(obj):
+        List.append(i)
+
+    return List
