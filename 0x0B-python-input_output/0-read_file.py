a4e61aa1fe7591e2b7ea7696aac97afe0a18794f
paxxchal <paschalgabrielacj8@gmail.com>
2023-01-16 15:53:12 +0100

    did first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+This module defines  a function
+that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
