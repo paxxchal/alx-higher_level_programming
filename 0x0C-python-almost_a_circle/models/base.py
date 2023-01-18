449fac043ff91cc783e43f0b647301561b999aa8
paxxchal <paschalgabrielacj8@gmail.com>
2023-01-18 21:14:13 +0100

    made some changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,9 +7,12 @@ first class Base
 
 
 class Base:
+    """This class is a base class"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """This function initializes attributes"""
         if id is not None:
             self.id = id
         else:
