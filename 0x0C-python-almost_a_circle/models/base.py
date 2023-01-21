78fbc2fa9727cb73571679b32b9140efcb6841b1
paxxchal <paschalgabrielacj8@gmail.com>
2023-01-21 17:10:54 +0100

    More json functions added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,3 +42,10 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, "w") as f:
             f.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
