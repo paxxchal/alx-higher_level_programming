61e19b40ff282de2267ef85c04d9d337fabe63b0
Paschal <59150676+paxxchal@users.noreply.github.com>
2023-09-06 09:35:56 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    matrix = [list(map(lambda x: x**2, row)) for row in matrix]
-    return matrix
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    a_dictionary = dict(sorted(a_dictionary.items()))
-    for i, j in a_dictionary.items():
-        print("{} : {}".format(i, j))
