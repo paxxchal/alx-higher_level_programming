eb37e12f19bc8e50bade331761f2def76c3c8023
Paschal <59150676+paxxchal@users.noreply.github.com>
2023-09-06 09:24:13 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/test.py
+++
@@ -1,8 +0,0 @@
-def uppercase(str):
-    for i in str:
-        if ord('a') <= ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-        print(f'{i}', end='')
-    print()
-
-uppercase("Best School 98 Battery street")
