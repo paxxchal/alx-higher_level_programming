fcb7239d276acd86560ea256f2f0abc7c6bc108e
paxxchal <paschalgabrielacj8@gmail.com>
2023-10-04 14:54:49 +0100

    Fixed some pycodestyle errors

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # This is a function that outputs a string in uppercase followed by a new line.
 def uppercase(str):
-    """This function takes a string as an argument and outputs it in uppercase."""
+    """takes a string as an argument and outputs it in uppercase."""
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:  # check if the character is lowercase
-            c = chr(
-                ord(c) - 32
-            )  # convert it to uppercase by subtracting 32 from its ASCII code
-        print("{}".format(c), end="")  # outputs the character without a new line
+        if ord(c) >= 97 and ord(c) <= 122:
+            # check if the character is lowercase
+            c = chr(ord(c) - 32)
+            # convert it to uppercase by subtracting 32 from its ASCII code
+        print("{}".format(c), end="")
+        # outputs the character without a new line
     print("")  # outputs a new line at the end
