2709feef40af2d888c7d4f21632eb13dd60ce5ca
paxxchal <paschalgabrielacj8@gmail.com>
2023-10-07 22:12:25 +0100

    Pushed a task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Initialize two variables to store the first and second elements of each tuple
+    a1 = 0
+    a2 = 0
+    b1 = 0
+    b2 = 0
+    # Check the length of each tuple and assign the values accordingly
+    if len(tuple_a) > 0:
+        a1 = tuple_a[0]
+        if len(tuple_a) > 1:
+            a2 = tuple_a[1]
+    if len(tuple_b) > 0:
+        b1 = tuple_b[0]
+        if len(tuple_b) > 1:
+            b2 = tuple_b[1]
+    # Add the corresponding elements of each tuple and return a new tuple
+    return (a1 + b1, a2 + b2)
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-print_list_integer = __import__("0-print_list_integer").print_list_integer
+add_tuple = __import__("7-add_tuple").add_tuple
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1,)))
+print(add_tuple(tuple_a, ()))
