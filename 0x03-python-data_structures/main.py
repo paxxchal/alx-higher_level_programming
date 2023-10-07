5dc16d371111bcc07ac413b1ea089403ec00c582
paxxchal <paschalgabrielacj8@gmail.com>
2023-10-07 23:31:54 +0100

    made some changes

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-add_tuple = __import__("7-add_tuple").add_tuple
+new_in_list = __import__("4-new_in_list").new_in_list
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
 
-print(add_tuple(tuple_a, (1,)))
-print(add_tuple(tuple_a, ()))
+print(new_list)
+print(my_list)
