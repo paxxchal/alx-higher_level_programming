68ca3f068dcbba6c6d629cf04fef0def6872a429
paxxchal <paschalgabrielacj8@gmail.com>
2024-01-25 02:05:41 +0100

    pushed a task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a class Square with a private instance attribute size."""
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+    - __size (int): The size of the square.
+
+    Methods:
+    - No methods defined for now.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+        - size (int): The size of the square.
+        """
+        self.__size = size
