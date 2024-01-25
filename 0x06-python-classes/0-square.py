c1537ed9315a3f2a3f094442a58cd93bd319f8b8
paxxchal <paschalgabrielacj8@gmail.com>
2024-01-25 02:01:41 +0100

    Pushed first task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines an empty class Square."""
+
+
+class Square:
+    """
+    This is an empty class Square.
+
+    Attributes:
+    - No attributes defined yet.
+
+    Methods:
+    - No methods defined yet.
+    """
+
+    pass
