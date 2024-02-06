72b8c0b1a5ad1445eaab4a94672f826e19b7f331
paxxchal <paschalgabrielacj8@gmail.com>
2024-02-06 18:47:42 +0100

    pushed a task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    pass
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-lookup = __import__("0-lookup").lookup
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
 
+bg = BaseGeometry()
 
-class MyClass1(object):
-    pass
-
-
-class MyClass2(object):
-    my_attr1 = 3
-
-    def my_meth(self):
-        pass
-
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
