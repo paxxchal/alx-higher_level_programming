708007e60bad2d20c2c6c87eb9fd413f52663a5b
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-02-07 09:46:05 +0100

    pushed a task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""Script to add arguments to a Python list and save them to a file."""
+"""
+Module 7-add_item
+Contains function that adds and saves to Python obj to JSON file; loads objects
+# run with ./7-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./7-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+"""
 
-import sys
-from os import path
 
-# Add the current directory to sys.path
-sys.path.append(path.0x0B-python-input_output(path.abspath(__file__)))
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+filename = "add_item.json"
 
-def add_item(args):
-    """Add arguments to a Python list and save them to a file.
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
 
-    Args:
-        args (list): List of arguments to add to the Python list.
-    """
-    filename = "add_item.json"
-
-    # Check if the file exists, if not, create it
-    if not path.exists(filename):
-        save_to_json_file([], filename)
-
-    # Load the existing list from the file
-    existing_list = load_from_json_file(filename)
-
-    # Add new arguments to the list
-    existing_list.extend(args)
-
-    # Save the updated list to the file
-    save_to_json_file(existing_list, filename)
-
-if __name__ == "__main__":
-    # Skip the first argument (script name) and pass the rest to add_item
-    arguments = sys.argv[1:]
-    add_item(arguments)
+save_to_json_file(existing_content + argv[1:], filename)
