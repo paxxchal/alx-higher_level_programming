f1a15910979824c287e251a0ab700af6279353c4
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-02-07 10:00:07 +0100

    fixed some bugs

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python3
+"""
+Script reads stdin line by line and computes metrics
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Each 10 lines and after a keyboard interruption (CTRL + C),
+prints those statistics since the beginning:
+total file size and
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+format: File size: <total size>
+format: <status code (in ascending order)>: <number>
+"""
+
+
 import sys
 
-file_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-count = 0
 
-try:
-    for line in sys.stdin:
-        count += 1
-        line = line.split()
-        try:
-            file_size += int(line[-1])
-        except (IndexError, ValueError):
-            pass
-        try:
-            status_codes[int(line[-2])] += 1
-        except (IndexError, ValueError):
-            pass
+def print_size_and_codes(size, stat_codes):
+    print("File size: {:d}".format(size))
+    for k, v in sorted(stat_codes.items()):
+        if v:
+            print("{:s}: {:d}".format(k, v))
 
-        if count % 10 == 0:
-            print("File size: {}".format(file_size))
-            for code in sorted(status_codes.keys()):
-                if status_codes[code] > 0:
-                    print("{}: {}".format(code, status_codes[code]))
 
-except KeyboardInterrupt:
-    print("File size: {}".format(file_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+def parse_stdin_and_compute():
+    size = 0
+    lines = 0
+    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                  "403": 0, "404": 0, "405": 0, "500": 0}
+    try:
+        for line in sys.stdin:
+            fields = list(map(str, line.strip().split(" ")))
+            size += int(fields[-1])
+            if fields[-2] in stat_codes:
+                stat_codes[fields[-2]] += 1
+            lines += 1
+            if lines % 10 == 0:
+                print_size_and_codes(size, stat_codes)
+    except KeyboardInterrupt:
+        print_size_and_codes(size, stat_codes)
+        raise
+
+    print_size_and_codes(size, stat_codes)
+
+
+parse_stdin_and_compute()
