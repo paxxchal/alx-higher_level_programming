2888cff66af3bc77137fc2dba9e5c854e49d1d0b
paxxchal <paschalgabrielacj8@gmail.com>
2024-04-10 07:00:09 +0100

    pushed a task

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Create a constant variable called myVar with the value "JavaScript is amazing"
-const myVar = 'JavaScript is amazing'
+const myVar = 'JavaScript is amazing';
 
 // Print the value of myVar using console.log
-console.log(myVar)
+console.log(myVar);
