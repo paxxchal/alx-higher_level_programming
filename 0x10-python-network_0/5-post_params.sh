dd43e9907723482bccdc3686fe9e6a4a3b40ee1c
paxxchal <paschalgabrielacj8@gmail.com>
2024-04-26 16:59:01 +0100

    pushed a task

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script sends a POST request with specified variables and displays the body of the response.
+curl -s -X POST -d email=test@gmail.com -d subject='I will always be here for PLD' "$1"
