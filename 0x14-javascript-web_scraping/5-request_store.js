b2c21da5b22ddf51c1db9e59ab922518c9f21954
paxxchal <paschalgabrielacj8@gmail.com>
2024-05-25 05:21:18 +0100

    fixed an error

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,25 +1,29 @@
 #!/usr/bin/node
-// Script that gets the contents of a webpage and stores it in a file.
-
 
 const request = require('request');
 const fs = require('fs');
 
+// Get the URL and file path from the arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
+// Check if both arguments are provided
+if (!url || !filePath) {
+  console.error('Usage: ./5-request_store.js <URL> <file path>');
+  process.exit(1);
+}
+
+// Make a GET request to the URL
 request(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
     return;
   }
 
-  fs.writeFile(filePath, body, 'utf8', (err) => {
+  // Write the response body to the file in utf-8 encoding
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
     if (err) {
       console.error(err);
-      return;
     }
-
-    console.log(`File saved to ${filePath}`);
   });
 });
