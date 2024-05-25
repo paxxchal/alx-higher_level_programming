729a25067f5135603602fdf7595ffdbde8bb8da3
paxxchal <paschalgabrielacj8@gmail.com>
2024-05-25 05:09:04 +0100

    pushed a task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-// Script that writes a string to a file.
-// The first argument is the file path.
-// The second argument is the string to write.
-// The content of the file must be written in utf-8.
-// If an error occurred during while writing, print the error object.
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const content = process.argv[3];
+const stringToWrite = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-    return;
+// Check if both arguments are provided
+if (!filePath || !stringToWrite) {
+  console.error('Usage: ./1-writeme.js <file path> <string to write>');
+  process.exit(1);
+}
+
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
   }
-
-  console.log(`Content written to ${filePath} successfully!`);
 });
