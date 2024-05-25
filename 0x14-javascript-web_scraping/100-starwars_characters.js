1437a831e7d97629e450f91fdcca2e4f76958fa4
paxxchal <paschalgabrielacj8@gmail.com>
2024-05-25 05:25:35 +0100

    fixed bugs

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,55 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie:
+
 const request = require('request');
 
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) throw error;
-  for (const character of JSON.parse(body).characters) {
-    request(character, function (error, response, body) {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
-    });
+// Get the Movie ID from the first argument
+const movieId = process.argv[2];
+
+// Check if the Movie ID argument is provided
+if (!movieId) {
+  console.error('Usage: ./100-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+// URL for the Star Wars API
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Make a request to the API URL
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  // Check if the request was successful
+  if (response.statusCode === 200) {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+
+    // Function to fetch and print character names
+    const fetchCharacterName = (characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (err, res, characterBody) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          if (res.statusCode === 200) {
+            const character = JSON.parse(characterBody);
+            console.log(character.name);
+            resolve();
+          } else {
+            reject(new Error(`Failed to fetch character. Status code: ${res.statusCode}`));
+          }
+        });
+      });
+    };
+
+    // Fetch and print all character names sequentially
+    characters.reduce((promise, characterUrl) => {
+      return promise.then(() => fetchCharacterName(characterUrl));
+    }, Promise.resolve());
+  } else {
+    console.log('Failed to retrieve the API. Status code:', response.statusCode);
   }
 });
