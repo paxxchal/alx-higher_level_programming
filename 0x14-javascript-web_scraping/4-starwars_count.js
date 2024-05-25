707aa03ab80ba4be8801f9759b9fb442684a9f03
paxxchal <paschalgabrielacj8@gmail.com>
2024-05-25 05:03:49 +0100

    fixed errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,41 @@
 #!/usr/bin/node
-// Script that prints the number of movies where the character “Wedge Antilles” is present.
 
 const request = require('request');
 
+// Get the API URL from the first argument
 const apiUrl = process.argv[2];
 
+// Check if the API URL argument is provided
+if (!apiUrl) {
+  console.error('Usage: ./4-starwars_count.js <API URL>');
+  process.exit(1);
+}
+
+// Character ID for Wedge Antilles
+const wedgeAntillesId = 18;
+
+// Make a request to the API URL
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
     return;
   }
 
-  const films = JSON.parse(body).results;
+  // Check if the request was successful
+  if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
 
-
-  const wedgeAntillesMovies = films.filter((film) => {
-    return film.characters.some((character) => {
-      return character.endsWith('/18/');
+    // Iterate over the films to count appearances of Wedge Antilles
+    films.forEach(film => {
+      const characters = film.characters;
+      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
+        count++;
+      }
     });
-  });
 
-  console.log(`Number of movies where Wedge Antilles is present: ${wedgeAntillesMovies.length}`);
+    console.log(count);
+  } else {
+    console.log('Failed to retrieve the API. Status code:', response.statusCode);
+  }
 });
